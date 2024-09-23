92e3c32ddf372882572f5f8395c793b6712bea84
Clemens Beißel <clemens-beissel@gmx.de>
2024-09-23 15:20:44 +0200

    Add https possibility for tools1

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import fs from "fs";
+import https from "https";
 
 const app = express();
-const server = http.createServer(app);
+let server;
+const publicServer = false;
+if (publicServer) {
+  //These files are autoupdated every 90 days on tools1
+  let options = {
+    key: fs.readFileSync("C:/Certbot/live/connect.blrk.io/privkey.pem"),
+    cert: fs.readFileSync("C:/Certbot/live/connect.blrk.io/fullchain.pem"),
+  };
+  server = https.createServer(options, app);
+} else {
+  server = http.createServer(app);
+}
 const io = new Server(server);
 
 const port = 3000;
